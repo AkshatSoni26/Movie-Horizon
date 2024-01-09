012103b25301657d5615bbb195b47bfd8f3621d7
AkshatSoni26 <135410361+AkshatSoni26@users.noreply.github.com>
2024-01-09 11:02:45 +0530

    add error pages

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,28 @@ import { FRONTENED_URL } from './url/url.js';
 import NowPlayingMoviesList from './Pages/NowPlayong/NowPlayingMoviesList.jsx';
 import SelectedMovie from './Pages/SelectedMovie/SelectedMovie.jsx';
 import Search from './Pages/search/Search.jsx';
+import Error from './Pages/error/Error.jsx'
 
 const routes = createBrowserRouter(
     createRoutesFromElements(
         [
-            <Route path={FRONTENED_URL.HOME} element={<Home />} />,
+            <Route path={FRONTENED_URL.HOME} element={<Home />} errorElement={<Error />}/>,
 
-            <Route path={FRONTENED_URL.NOW_PLAYING} element={<NowPlayingMoviesList />} />,
-            <Route path={FRONTENED_URL.POPULAR} element={<NowPlayingMoviesList />} />,
-            <Route path={FRONTENED_URL.TOP_RATED} element={<NowPlayingMoviesList />} />,
-            <Route path={FRONTENED_URL.UPCOMING} element={<NowPlayingMoviesList />} />,
-            <Route path={FRONTENED_URL.SIMILAR} element={<NowPlayingMoviesList />} />,
-            
-            <Route path={`${FRONTENED_URL.SELECTED_MOVIE}/:id` } element={<SelectedMovie />} />,
-            <Route path={FRONTENED_URL.SEARCH } element={<Search />} />,
+            <Route path={FRONTENED_URL.NOW_PLAYING} element={<NowPlayingMoviesList />} errorElement={<Error />}/>,
+            <Route path={FRONTENED_URL.POPULAR} element={<NowPlayingMoviesList />} errorElement={<Error />}/>,
+            <Route path={FRONTENED_URL.TOP_RATED} element={<NowPlayingMoviesList />} errorElement={<Error />}/>,
+            <Route path={FRONTENED_URL.UPCOMING} element={<NowPlayingMoviesList />} errorElement={<Error />}/>,
+            <Route path={FRONTENED_URL.SIMILAR} element={<NowPlayingMoviesList />} errorElement={<Error />}/>,
+
+            <Route path={`${FRONTENED_URL.SELECTED_MOVIE}/:id`} element={<SelectedMovie />} errorElement={<Error />}/>,
+            <Route path={FRONTENED_URL.SEARCH} element={<Search />} errorElement={<Error />}/>,
         ]
     )
 )
 
 function App() {
     return (
-       <RouterProvider router={routes} />
+        <RouterProvider router={routes} />
     )
 }
 
---
+++ src/Pages/error/Error.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Error() {
+  return (
+    <div style={{width:'100vw', height:'100vh'}} className='d-flex justify-content-center align-items-center'>Something went wrong. 😬</div>
+  )
+}
+
+export default Error
